5e46743fca59ddebf3c28e9e03b329e63be8b150
seiyadaiki <seiyadaiki@gmail.com>
2020-11-05 18:08:01 +0900

    単体テストコード実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,11 +7,12 @@ class User < ApplicationRecord
   has_many :itmes
   has_many :orders
   with_options presence: true do
-    validates :nickname
-    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
-    validates :family_name_frigana, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
-    validates :first_name_frigana, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
+    validates :nickname, length: { maximum: 40 }
+    validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)\w{6,12}\z/, message: 'is invalid. Input half-width characters.' }
+    validates :family_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'is invalid. Input full-width characters.' }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: 'is invalid. Input full-width characters.' }
+    validates :family_name_frigana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters.' }
+    validates :first_name_frigana, format: { with: /\A[ァ-ヶー－]+\z/, message: 'is invalid. Input full-width katakana characters.' }
     validates :birthday
   end
 end
