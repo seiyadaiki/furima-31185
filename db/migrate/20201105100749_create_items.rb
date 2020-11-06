4ecf9ecaa4b05c402dd5b13c5d4c3472e1e65c78
seiyadaiki <seiyadaiki@gmail.com>
2020-11-06 12:03:04 +0900

    ActiveHash内のデータ実装

--- db/migrate/20201105100749_create_items.rb
+++ db/migrate/20201105100749_create_items.rb
@@ -1,8 +1,14 @@
 class CreateItems < ActiveRecord::Migration[6.0]
   def change
     create_table :items do |t|
-      
-      t.timestamps
+      t.string :title,                 null: false
+      t.text :explanation,             null: false
+      t.integer :category_id,          null: false
+      t.integer :status_id,            null: false
+      t.integer :shipping_fee_id,      null: false
+      t.integer :location_id,          null: false
+      t.integer :delivery_time_id,     null: false
+      t.references :user,              null: false, foreign_key: true
     end
   end
 end
