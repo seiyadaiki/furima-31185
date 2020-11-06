8643c427cedbb9fb65593c84bc7b117507e9ea37
seiyadaiki <seiyadaiki@gmail.com>
2020-11-06 16:49:40 +0900

    itemモデルのバリデーション実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -9,4 +9,19 @@ class Item < ApplicationRecord
   belongs_to :shipping_fee
   belongs_to :location
   belongs_to :delivery_time
+
+  with_options presence: true do
+    validates :title, length: { maximum: 40 }
+    validates :explanation, length: { maximum: 1000 }
+    validates :selling_price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999 }
+  end
+
+  with_options numericality: { other_than: 0} do
+    validates :category_id
+    validates :status_id
+    validates :shipping_fee_id
+    validates :location_id
+    validates :delivery_time_id
+  end
+
 end
