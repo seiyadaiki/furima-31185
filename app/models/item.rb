04b7551afceece018f28c4be5c01ad09d957cda8
seiyadaiki <seiyadaiki@gmail.com>
2020-11-09 19:52:36 +0900

    販売手数料と販売利益の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,12 +14,11 @@ class Item < ApplicationRecord
     validates :image
     validates :title, length: { maximum: 40 }
     validates :explanation, length: { minimum: 7, maximum: 1000 }
-    validates :selling_price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999, message: "is out of setting range" }
-    validates :category_id,  numericality: { other_than: 0, message: "can't be blank" }
+    validates :selling_price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, message: 'is out of setting range' }
+    validates :category_id, numericality: { other_than: 0, message: "can't be blank" }
     validates :status_id, numericality: { other_than: 0, message: "can't be blank" }
     validates :shipping_fee_id, numericality: { other_than: 0, message: "can't be blank" }
     validates :location_id, numericality: { other_than: 0, message: "can't be blank" }
     validates :delivery_time_id, numericality: { other_than: 0, message: "can't be blank" }
   end
-
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -10,66 +10,66 @@ describe Item do
         expect(@item).to be_valid
       end
     end
-    context "出品ができない場合" do
-      it "titleが空だとできない" do
+    context '出品ができない場合' do
+      it 'titleが空だとできない' do
         @item.title = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Title can't be blank")
       end
-      it "imageが空だとできない" do
+      it 'imageが空だとできない' do
         @item.image = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Image can't be blank")
       end
-      it "explanationが空だとできない" do
+      it 'explanationが空だとできない' do
         @item.explanation = ''
         @item.valid?
         expect(@item.errors.full_messages).to include("Explanation can't be blank")
       end
-      it "expanationが6文字以下だとできない" do
+      it 'expanationが6文字以下だとできない' do
         @item.explanation = 'aaaaaa'
         @item.valid?
-        expect(@item.errors.full_messages).to include("Explanation is too short (minimum is 7 characters)")
+        expect(@item.errors.full_messages).to include('Explanation is too short (minimum is 7 characters)')
       end
-      it "category_idが0だとできない" do
+      it 'category_idが0だとできない' do
         @item.category_id = 0
         @item.valid?
         expect(@item.errors.full_messages).to include("Category can't be blank")
       end
-      it "status_idが0だとできない" do
+      it 'status_idが0だとできない' do
         @item.status_id = 0
         @item.valid?
         expect(@item.errors.full_messages).to include("Status can't be blank")
       end
-      it "shippng_fee_idが0だとできない" do
+      it 'shippng_fee_idが0だとできない' do
         @item.shipping_fee_id = 0
         @item.valid?
         expect(@item.errors.full_messages).to include("Shipping fee can't be blank")
       end
-      it "location_idが0だとできない" do
+      it 'location_idが0だとできない' do
         @item.location_id = 0
         @item.valid?
         expect(@item.errors.full_messages).to include("Location can't be blank")
       end
-      it "delivery_time_idが0だとできない" do
+      it 'delivery_time_idが0だとできない' do
         @item.delivery_time_id = 0
         @item.valid?
         expect(@item.errors.full_messages).to include("Delivery time can't be blank")
       end
-      it "selling_priceが空だとできない" do
+      it 'selling_priceが空だとできない' do
         @item.selling_price = nil
         @item.valid?
         expect(@item.errors.full_messages).to include("Selling price can't be blank")
       end
-      it "selling_priceが299以下だとできない" do
+      it 'selling_priceが299以下だとできない' do
         @item.selling_price = 299
         @item.valid?
-        expect(@item.errors.full_messages).to include("Selling price is out of setting range")
+        expect(@item.errors.full_messages).to include('Selling price is out of setting range')
       end
-      it "selling_priceが10000000以上だとできない" do
-        @item.selling_price = 10000000
+      it 'selling_priceが10000000以上だとできない' do
+        @item.selling_price = 10_000_000
         @item.valid?
-        expect(@item.errors.full_messages).to include("Selling price is out of setting range")
+        expect(@item.errors.full_messages).to include('Selling price is out of setting range')
       end
     end
   end
