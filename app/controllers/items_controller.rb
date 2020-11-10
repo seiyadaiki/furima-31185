40a01855a8b1ced429d3749f29ffb87f254de7cd
seiyadaiki <seiyadaiki@gmail.com>
2020-11-10 20:21:49 +0900

    商品削除機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :edit]
-  before_action :set_item, only: [:edit, :show, :update]
+  before_action :set_item, only: [:edit, :show, :update, :destroy]
   before_action :move_to_index, only: :edit
   
 
@@ -32,6 +32,12 @@ class ItemsController < ApplicationController
       redirect_to item_path 
     else
       render :edit
+    end
+  end
+
+  def destroy
+    if @item.destroy
+      redirect_to root_path
     end
   end
 
