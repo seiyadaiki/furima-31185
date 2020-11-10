000dc6fd1c96b88f636c9debfe4126c4db1950c1
seiyadaiki <seiyadaiki@gmail.com>
2020-11-10 19:09:48 +0900

    itemコントローラーのリファクタリング

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :edit]
   before_action :move_to_index, only: :edit
+  before_action :set_item, only: [:edit, :show, :update]
 
   def index
     @items = Item.all.order('created_at DESC')
@@ -20,17 +21,14 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
-      redirect_to item_path method: :get
+      redirect_to item_path 
     else
       render :edit
     end
@@ -47,5 +45,9 @@ class ItemsController < ApplicationController
     unless @item.user == current_user
       redirect_to action: :index
     end
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
