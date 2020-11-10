5af034cb27fa1b8e29dc325a9212971bce33f763
seiyadaiki <seiyadaiki@gmail.com>
2020-11-10 12:18:48 +0900

    商品を降順で一覧表示させた

--- db/schema.rb
+++ db/schema.rb
@@ -43,6 +43,8 @@ ActiveRecord::Schema.define(version: 2020_11_05_101432) do
     t.integer "delivery_time_id", null: false
     t.integer "selling_price", null: false
     t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_items_on_user_id"
   end
 
