1b36fb4d58fba03aec059407cd0f3fc8a83a190f
seiyadaiki <seiyadaiki@gmail.com>
2020-11-11 19:22:39 +0900

    バリデーション記述まで

---
+++ app/models/order_adress.rb
@@ -0,0 +1,16 @@
+class OrderAdress
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture, :city, :house_number, :building_name, :phone_number
+
+  with_options presence: true do
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :prefecture_id, numericality: { other_than: 0, message: "can't be blank" }
+    validetes :city
+    validates :house_number
+    validetes :phone_number, format: {with: /\A\d{11}\z/, message: "is invalid. Input half-integer characters"}
+  end
+
+  def save
+    Order.create( user_id: user.id, item_id: item.id)
+    Address.create(prefecture: prefecture, city: city, house_number: house_number, building_name: building_name, phone_number: phone_number)
+end
