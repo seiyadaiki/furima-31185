887042dfa80a355fb5e8118e44234ff55e08b91e
seiyadaiki <seiyadaiki@gmail.com>
2020-11-12 11:05:32 +0900

    モデル、テーブルカラムの作成

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,7 @@
 class OrdersController < ApplicationController
 
   def index
+    @orders_adresses = OrderAdress.all
   end
 
   def create
--- app/models/adress.rb
+++ app/models/adress.rb
@@ -1,3 +1,5 @@
 class Adress < ApplicationRecord
   belongs_to :order
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
 end
