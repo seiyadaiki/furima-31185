3123e6517fc17d24d08e48fb3142ddea94ade16b
seiyadaiki <seiyadaiki@gmail.com>
2020-11-18 15:42:53 +0900

    Basic認証

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :basic_auth
 
   private
 
@@ -10,5 +11,11 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:family_name_frigana])
     devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name_frigana])
     devise_parameter_sanitizer.permit(:sign_up, keys: [:birthday])
+  end
+
+  def basic_auth
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"] 
+    end
   end
 end
